e89f31c0ec5cf764fe8523ef1127aeb8b5c56c9c
Gevorg <gevnersisyan03@gmail.com>
2022-08-31 15:33:17 +0400

    changed save function

--- source/file.py
+++ source/file.py
@@ -9,6 +9,7 @@ class File(QMainWindow):
     def __init__(self):
         super(File, self).__init__()
         self.filename = ''
+        is_saved = False
         #self.obj = PhotoshopEditor()
 
 
@@ -25,15 +26,16 @@ class File(QMainWindow):
 
 
     def save(self, obj):
-        try:
-            if self.fileName == '':
-                self.fileName = 'C:/Users/m_sor/Desktop/untitled.png'
-        except:
-            self.fileName = 'C:/Users/m_sor/Desktop/untitled.png'
-        print(self.fileName)
-        fileFormat = self.fileName[len(self.fileName) - 4:]
-        initialPath = self.fileName
-        print(self.fileName)
+        if self.fileName == '':
+            return
+
+        lst = str(self.fileName).split('/')
+        image_name = str(lst[-1])
+        fileFormat = image_name[len(image_name) - 3:]
+        image_name = image_name[:len(image_name) - 3]
+        print(fileFormat)
+        initialPath = self.fileName + f'/{image_name}.' + fileFormat
+
         fileName, _ = QFileDialog.getSaveFileName(obj, "Save", initialPath,
                 "%s Files (*.%s);;All Files (*)" % (fileFormat.upper(), fileFormat))
         if fileName:
