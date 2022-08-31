eef88b3ff649ac8c86dd13fde00fa573b515d03c
Gevorg <gevnersisyan03@gmail.com>
2022-08-31 14:59:34 +0400

    created save

--- source/file.py
+++ source/file.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import QMessageBox, QFileDialog, QWidget, QMainWindow
 class File(QMainWindow):
     def __init__(self):
         super(File, self).__init__()
+        self.filename = ''
         #self.obj = PhotoshopEditor()
 
 
@@ -16,20 +17,31 @@ class File(QMainWindow):
 
 
     def open(self,obj):
-        fileName, _ = QFileDialog.getOpenFileName(obj, "Open File", QDir.currentPath(),
+        self.fileName, _ = QFileDialog.getOpenFileName(obj, "Open File", QDir.currentPath(),
                                                   "Image files (*.jpg *.png)")
-        if fileName:
-            obj.scribbleArea.openImage(fileName)
+        if self.fileName:
+            obj.scribbleArea.openImage(self.fileName)
 
 
 
     def save(self, obj):
-        #from source.photoshop_editor import PhotoshopEditor
-        #obj = PhotoshopEditor()
-        print(obj)
-        obj.button_list[1].setStyleSheet('background-color: blue;')
+        try:
+            if self.fileName == '':
+                self.fileName = 'C:/Users/m_sor/Desktop/untitled.png'
+        except:
+            self.fileName = 'C:/Users/m_sor/Desktop/untitled.png'
+        print(self.fileName)
+        fileFormat = self.fileName[len(self.fileName) - 4:]
+        initialPath = self.fileName
+        print(self.fileName)
+        fileName, _ = QFileDialog.getSaveFileName(obj, "Save", initialPath,
+                "%s Files (*.%s);;All Files (*)" % (fileFormat.upper(), fileFormat))
+        if fileName:
+            return obj.scribbleArea.saveImage(fileName, fileFormat)
 
-    def save_as(self,obj):
+        return False
+
+    def save_as(self, obj):
         pass
 
 
