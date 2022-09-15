e8813ac13e0b9b873c2e7c858226a81054e64f05
martun <martun.davitavyan@mail.ru>
2022-09-15 16:27:40 +0400

    crop, cut, copy, paste,  marquee ... updated

--- source/scribble_area.py
+++ source/scribble_area.py
@@ -1,12 +1,15 @@
 import io
 import sys
 
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import QPoint, Qt, QSize, QBuffer, QRect
-from PyQt5.QtGui import QImage, qRgb, QPainter, QPen, QColor, QIcon, QTextCharFormat, QFont, QBrush, QPolygon
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import QPoint, Qt, QSize, QBuffer, QRect, QThread, QFile, QIODevice
+from PyQt5.QtGui import QImage, qRgb, QPainter, QPen, QColor, QIcon, QTextCharFormat, QFont, QBrush, QPolygon, QPixmap
 from PyQt5.QtWidgets import QWidget, QColorDialog, QInputDialog, QUndoStack, QApplication
 import numpy as np
 import cv2 as cv
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PIL import Image, ImageFilter
 from source.edit import UndoCommand
 from source.buttons import Buttons
 from source.buttons import TextTypeCheckbox
@@ -20,8 +23,8 @@ class ScribbleArea(QWidget):
         self.image = QImage()
         self.buttons = Buttons()
         self.pressed_button = None
-        newSize = self.image.size().expandedTo(self.size())
-        self.resizeImage(self.image, QSize(newSize))
+        # newSize = self.image.size().expandedTo(self.size())
+        # self.resizeImage(self.image, QSize(newSize))
         self.update()
         self.a = ''
         self.lastPoint = QPoint()
@@ -42,6 +45,14 @@ class ScribbleArea(QWidget):
         self.coords = ()
         self.shape = QRect()
         self.coordinates = []
+        self.imageDraw = QtGui.QImage(self.size(), QtGui.QImage.Format_ARGB32)
+        self.image = QImage(QtGui.QImage(self.size(), QtGui.QImage.Format_ARGB32))
+        self.image.fill(qRgb(255,255,255))
+        self.update()
+        self.brushSize = 2
+        self._clear_size = 20
+        self.brushColor = QtGui.QColor(QtCore.Qt.black)
+        self.change, self.open = False, False
 
     def is_pressed(self, value):
         self.pressed = value
@@ -51,6 +62,7 @@ class ScribbleArea(QWidget):
         return self.width(), self.height()
 
     def openImage(self, img):
+        self.open = True
         # newSize = img.size().expandedTo(self.size())
         newSize = QSize(img.shape[0], img.shape[1])
         self.resizeImage(img, newSize)
@@ -61,7 +73,6 @@ class ScribbleArea(QWidget):
     def resizeImage(self, image, newSize):
         # if image.size() == newSize:
         # return
-
         newImage = QImage(newSize, QImage.Format_RGB32)
         newImage.fill(qRgb(255, 255, 255))
         painter = QPainter(newImage)
@@ -74,15 +85,43 @@ class ScribbleArea(QWidget):
         # newWidth = max(self.width() + 128, self.image.width())
         # newHeight = max(self.height() + 128, self.image.height())
         # self.resizeImage(self.image, QSize(self.width(), self.height()))
-        img = cv.resize(self.qimage_to_cv(self.image), self.current_window_size())
-        self.openImage(img)
-        self.image = self.cv_to_qimage(img)
+        if self.open:
+
+            img = cv.resize(self.qimage_to_cv(self.image), self.current_window_size())
+            # self.openImage(img)
+            self.image = self.cv_to_qimage(img)
+        else:
+            pixmap = QPixmap()
+            pixamp2 = pixmap.fromImage(self.image.copy().scaled(self.width(), self.height(), Qt.IgnoreAspectRatio,
+                                                                    Qt.SmoothTransformation))
+            self.imagecopy = self.image.copy()
+
+            self.image = pixamp2.toImage()
+
+
+        pixmap = QPixmap()
+        pixamp2 = pixmap.fromImage(self.imageDraw.copy().scaled(self.width(), self.height(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
+        self.imagecopy = self.image.copy()
+
+        self.imageDraw = pixamp2.toImage()
         self.update()
 
         super(ScribbleArea, self).resizeEvent(event)
 
     def saveImage(self, fileName, fileFormat):
-        visibleImage = self.image
+
+        frontImage = self.qimage_to_pil(self.imageDraw).convert("RGBA")
+
+        background = self.qimage_to_pil(self.image).convert("RGBA")
+
+        width = (background.width - frontImage.width) // 2
+
+        height = (background.height - frontImage.height) // 2
+
+        background.paste(frontImage, (width, height), frontImage)
+
+        visibleImage = self.pil_to_qimage(background)
+
         self.resizeImage(visibleImage, self.size())
 
         if visibleImage.save(fileName, fileFormat):
@@ -93,23 +132,26 @@ class ScribbleArea(QWidget):
         return self.image
 
     def paintEvent(self, event):
-        painter = QPainter(self)
+        painter = QtGui.QPainter(self)
         dirtyRect = event.rect()
         painter.drawImage(dirtyRect, self.cv_to_qimage(self.image), dirtyRect)
+        painter.drawImage(dirtyRect, self.imageDraw, dirtyRect)
         painter.drawText(150, 250, self.text)
         self.update()
         if self.pressed_button == "paint":
-            painter.drawImage(dirtyRect, self.cv_to_qimage(self.image), dirtyRect)
+            painter.drawImage(dirtyRect, self.imageDraw, dirtyRect)
             painter.drawText(150, 250, self.text)
         if self.pressed_button == "marquee":
             painter.setPen(QPen(Qt.black, 1, Qt.DotLine))
-            painter.drawImage(dirtyRect, self.cv_to_qimage(self.image), dirtyRect)
+            painter.drawImage(dirtyRect, self.imageDraw, dirtyRect)
             painter.drawText(150, 250, self.text)
             if not self.begin.isNull() and not self.end.isNull():
-                self.shape = QRect(self.begin,self.end)
-                #self.coords = self.shape.getCoords()
-                #painter.drawRect(self.shape.normalized())
+                self.shape = QRect(self.begin, self.end)
+                # self.coords = self.shape.getCoords()
+                # painter.drawRect(self.shape.normalized())
                 painter.drawRect(QRect(self.begin, self.end).normalized())
+        if self.pressed_button == "eraser":
+            pass
 
     def mousePressEvent(self, event):
         self.make_undo_command()
@@ -123,10 +165,12 @@ class ScribbleArea(QWidget):
                 self.begin = self.end = event.pos()
                 self.update()
                 super().mousePressEvent(event)
+            if self.pressed_button == "eraser":
+                pass
 
     def mouseMoveEvent(self, event):
         if self.pressed_button == 'paint':
-            painter = QPainter(self.image)
+            painter = QPainter(self.imageDraw)
             painter.setPen(QPen(
                 QColor(self.color_pen[0], self.color_pen[1], self.color_pen[2], self.color_pen[3]),
                 self.width_pen, Qt.SolidLine))
@@ -142,19 +186,21 @@ class ScribbleArea(QWidget):
             super().mouseMoveEvent(event)
 
         if self.pressed_button == "eraser":
-            self.testimage = QImage("../../../../martun/Downloads/jpeg-home.jpg")
-
-            #painter.setBrush(QBrush(Qt.black, Qt.BDiagPattern))
-            print((self.lastPoint))
+            painter = QtGui.QPainter(self.imageDraw)
+            painter.setPen(QtGui.QPen(self.brushColor, self.brushSize,
+                                      QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin))
+            r = QtCore.QRect(QtCore.QPoint(), self._clear_size * QtCore.QSize())
+            r.moveCenter(event.pos())
+            painter.save()
+            painter.setCompositionMode(QPainter.CompositionMode_Clear)
+            painter.eraseRect(r)
+            painter.restore()
+            painter.end()
             self.lastPoint = event.pos()
-            self.image.setPixel(self.lastPoint,qRgb(0, 0, 0))
-            if self.lastPoint not in self.coordinates:
-                self.coordinates.append(self.lastPoint)
-            for i in self.coordinates:
-                self.image.setPixel(i,qRgb(0, 0, 0))
+            self.update()
 
-
-
+            self.lastPoint = event.pos()
+            self.update()
             self.update()
             self.check = True
 
@@ -180,10 +226,8 @@ class ScribbleArea(QWidget):
         if self.underline:
             pass
 
-
     def text_type(self):
         TextTypeCheckbox(self).exec()
-
 
     def text_width(self):
         num, ok = QInputDialog.getInt(self, "Text width", "Choose the text width")
@@ -211,3 +255,18 @@ class ScribbleArea(QWidget):
             qimg.loadFromData(io_buf.getvalue())
             return qimg
         return img
+
+    def qimage_to_pil(self, img):
+        buffer = QBuffer()
+        buffer.open(QBuffer.ReadWrite)
+        img.save(buffer, "PNG")
+        pil_im = Image.open(io.BytesIO(buffer.data()))
+        return pil_im
+
+    def pil_to_qimage(self, img):
+        bytes_img = io.BytesIO()
+        img.save(bytes_img, format='PNG')
+
+        qimg = QImage()
+        qimg.loadFromData(bytes_img.getvalue())
+        return qimg
