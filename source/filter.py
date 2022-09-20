4af6469c47b09f9766e42554d621eec813136d49
martun <martun.davitavyan@mail.ru>
2022-09-20 16:02:00 +0400

    crop, cut, copy, paste,  marquee ... updated

--- source/filter.py
+++ source/filter.py
@@ -15,26 +15,10 @@ import cv2 as cv
 
 class Filter():
     def __init__(self):
-        super(Filter, self).__init__()
-        self.obj = self
-
-    def qimage_to_cv(self, img: QImage):
-        buffer = QBuffer()
-        buffer.open(QBuffer.ReadWrite)
-        img.save(buffer, "PNG")
-        img_stream = io.BytesIO((buffer.data()))
-        img = cv.imdecode(np.frombuffer(img_stream.read(), np.uint8), 1)
-        return img
-
-    def cv_to_qimage(self, img):
-        is_success, buffer = cv.imencode(".jpg", img)
-        io_buf = io.BytesIO(buffer)
-        qimg = QImage()
-        qimg.loadFromData(io_buf.getvalue())
-        return qimg
+        super().__init__()
 
     def blur(self, obj):
-        img = obj.scribbleArea.image
+        img = obj.scribble_area.image
         buffer = QBuffer()
         buffer.open(QBuffer.ReadWrite)
         img.save(buffer, "PNG")
@@ -46,30 +30,52 @@ class Filter():
         qimg = QImage()
         qimg.loadFromData(bytes_img.getvalue())
 
-        obj.scribbleArea.image = qimg
-        obj.scribbleArea.update()
+        obj.scribble_area.image = qimg
+        obj.scribble_area.update()
 
     def noise(self, obj):
-        img = Filter().qimage_to_cv(obj.scribbleArea.image)
+        img = obj.scribble_area.QimageToCv(obj.scribble_area.image)
 
         row, col, ch = img.shape
         gauss = np.random.randn(row, col, ch)
         gauss = gauss.reshape(row, col, ch)
         speckle_noisy = img + img * gauss
 
-        qimg = Filter().cv_to_qimage(speckle_noisy)
-        obj.scribbleArea.image = qimg
-        obj.scribbleArea.update()
+        qimg = obj.scribble_area.CvToQimage(speckle_noisy)
+        obj.scribble_area.image = qimg
+        obj.scribble_area.update()
 
-    def distort(self, obj):
-        pass
+    def twirling_spirals(self, obj):
+        im = obj.scribble_area.QimageToCv(obj.scribble_area.image)
+        cx = im.shape[1] / 2
+        cy = im.shape[0] / 2
+        a = -1
+        b = 2
+        c = 1
+        r = 1
 
+        x = np.linspace(0, im.shape[1], im.shape[1], dtype=np.float32)
+        y = np.linspace(0, im.shape[0], im.shape[0], dtype=np.float32)
+        xv, yv = np.meshgrid(x - cx, y - cy)
+
+        mag, ang = cv.cartToPolar(xv, yv)
+        nmag = cv.normalize(mag, None, norm_type=cv.NORM_MINMAX)
+
+        sxv, syv = cv.polarToCart(mag, (ang + (a + b * np.pi * nmag ** (1.0 / c)) * (nmag < r)))
+        spiral = cv.remap(im,
+                          sxv + cx,
+                          syv + cy,
+                          cv.INTER_LINEAR)
+
+        qimg = obj.scribble_area.CvToQimage(spiral)
+        obj.scribble_area.image = qimg
+        obj.scribble_area.update()
     def pixelate(self, obj):
-        img = Filter().qimage_to_cv(obj.scribbleArea.image)
+        img = obj.scribble_area.QimageToCv(obj.scribble_area.image)
         height, width = img.shape[:2]
         w, h = (16, 16)
         temp = cv.resize(img, (w, h), interpolation=cv.INTER_LINEAR)
         output = cv.resize(temp, (width, height), interpolation=cv.INTER_NEAREST)
-        qimg = Filter().cv_to_qimage(output)
-        obj.scribbleArea.image = qimg
-        obj.scribbleArea.update()
+        qimg = obj.scribble_area.CvToQimage(output)
+        obj.scribble_area.image = qimg
+        obj.scribble_area.update()
