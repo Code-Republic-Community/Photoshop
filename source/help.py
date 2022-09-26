913fbfabf29782e851ef6677541c6d5814ee92a2
martun <martun.davitavyan@mail.ru>
2022-09-26 17:54:14 +0400

    crop, cut, copy, paste,  marquee ... updated

--- source/help.py
+++ source/help.py
@@ -33,7 +33,7 @@ class Help(QDialog):
         self.label_4.setSizePolicy(sizePolicy)
         font = QtGui.QFont()
         font.setFamily("Arial")
-        font.setPointSize(18)
+        font.setPointSize(14)
         font.setItalic(True)
         self.label_4.setFont(font)
         self.label_4.setLayoutDirection(QtCore.Qt.LeftToRight)
@@ -58,17 +58,25 @@ class Help(QDialog):
         self.label_4.setText(_translate("Dialog", "It\'s a Photoshop alternative that you can use. Photoshop interface and \n"
 "photo editing feature tools are available on it."))
 
-        dictionary_shortcuts = {'New': 'Ctrl+N', 'Open': 'Ctrl+O', 'Save': 'Ctrl+S',
-                                'Save As': 'Ctrl+Shift+S', 'Print': 'Ctrl+P', 'Close': 'Ctrl+W',
-                                'Undo': 'Ctrl+Z', 'Redo': 'Ctrl+Y', 'Cut': 'Ctrl+X', 'Copy': 'Ctrl+C',
-                                'Paste': 'Ctrl+V', 'Clear screen': 'Ctrl+L',
-                                'Keyboard shortcuts': 'Ctrl+K', 'Image size': 'Ctrl+Alt+I',
-                                'Canvas size': 'Ctrl+Alt+C', 'Rotate left': 'Shift+Ctrl+L',
-                                'Rotate right': 'Shift+Ctrl+R', 'Blur': 'Shift+Ctrl+B',
-                                'Noise': 'Shift+Ctrl+N', 'Twirling spirals': 'Shift+Ctrl+P',
-                                'Pixelate': 'Shift+Ctrl+P', 'Help': 'Ctrl+H', 'Documentation': 'Ctrl+D'}
+        dictionary_shortcuts = [
+            'New : New blank window', 'Open : Open an image file.' , 'Save : Replace the uploaded image with a new image' ,
+            'Save as : Save the image on screen into the selected file format' , 'Print : Print the picture that you are currently viewing',
+            'Close : Quit the program' , 'Undo : It erases the last change done to the image' , 'Redo : Restores something undone' ,
+            'Cut : The removal of unwanted outer areas from a image' , 'Copy : Copying part of an image' , 'Paste a copied area on your image',
+            'Clear screen : Clears the scribble area' , 'Keyboard shortcuts : Show the list of shortcuts' , 'Image size : Changes the image size' ,
+            'Canvas size : Changes the canvas size' , 'Rotate left : Rotates the image 90 degrees to the left', 'Rotate right : Rotates the image 90 degrees to the right',
+            'Blur : Blur refers to the part of an image that is out of focus', 'Noise : Random variation of brightness or color information in images',
+            'Twirling spirals :  Applies a swirling pattern to the image', 'Pixelate : Cause an image to break up into pixels, as by oversizing the image',
+            'Help : Help window', 'Documentation : Software documentation', 'Paint : Paint on the image, also you can choose the width and color',
+            'Move : Move the text or a copied image', 'Select : Select a part of an image', 'Lasso : Lasso select erases the selected area',
+            'Crop : Crops the image and keeps only the selected part', 'Eyedropper : Click a point in the image to identify and select its color',
+            'Eraser : You can choose the transparent eraser or the rubber', 'Text : Put a text to the image' ,
+            'Convert : Convert any image to a different format'
 
-        for key in dictionary_shortcuts.keys():
+
+        ]
+
+        for key in dictionary_shortcuts:
             my_font = QtGui.QFont()
             my_font.setFamily("Arial")
             my_font.setPointSize(14)
